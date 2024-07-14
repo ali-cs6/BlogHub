bb7fe9905ea466ab73fba298011fb9ecf02b94f0
Ali <alicode6.cs@gmail.com>
2024-07-14 16:00:29 +0500

    Initiated Blog publishing Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Creatng My Own App for the first, Hue Hue Hue </h1>
+    </>
+  )
+}
+
+export default App
