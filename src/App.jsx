444b8c32f4d245bbb8a3a0fae7eb8043e44d83d3
Ali <alicode6.cs@gmail.com>
2024-07-29 17:38:11 +0500

    set Environment Variables and configured them

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Creatng My Own App for the first, Hue Hue Hue </h1>
+      <h1>BLOG writing Project With APPWRITE</h1>
     </>
   )
 }
---
+++ src/configEnvVar/config.js
@@ -0,0 +1,11 @@
+// best practice with envornment variables(access) to avoid crashes and errors
+const config = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+
+export default config;
